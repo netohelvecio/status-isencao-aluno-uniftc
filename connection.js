49f10c774f08e49ee52f7fcbd62ce53a80fde37a
Helvécio Neto <helvecioneto77@gmail.com>
2020-05-08 08:34:15 -0300

    Adicionando dotenv

--- connection.js
+++ connection.js
@@ -7,7 +7,7 @@ const connection = knex({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     server: process.env.DB_SERVER,
-    database: process.env.LYCEUMINTEGRACAO,
+    database: process.env.DB_DATABASE,
   }
 });
 
